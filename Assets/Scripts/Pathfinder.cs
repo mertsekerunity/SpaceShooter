a80171de6368d22213b10593a5edbedb42245a1b
mertsekerunity <mertseker9393@gmail.com>
2025-01-17 01:24:43 +0100

    alternative follow path added.
    perlin noise added.
    alternative follow path added.

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
     List<Transform> waypoints;
-    int waypointIndex = 0;
+    //int waypointIndex = 0;
+    int startingWaypointIndex = 0; // alternative method
+    int waypointIndex = 1; // alternative method
+    float perlinNoiseScale = 1.0f; //alternative method
 
     EnemySpawner enemySpawner;
     WaveConfigSO waveConfig;
@@ -20,13 +24,15 @@ public class Pathfinder : MonoBehaviour
     {
         waveConfig = enemySpawner.GetCurrentWave();
         waypoints = waveConfig.GetWayPoints();
-        transform.position = waypoints[waypointIndex].position;
+        //transform.position = waypoints[waypointIndex].position;
+        transform.position = waypoints[startingWaypointIndex].position; // alternative method
     }
 
     // Update is called once per frame
     void Update()
     {
-        FollowPath();
+        //FollowPath();
+        FollowPathAlternative();
     }
 
     void FollowPath()
@@ -46,5 +52,30 @@ public class Pathfinder : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void FollowPathAlternative() //alternative method
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            float moveSpeed = waveConfig.GetMoveSpeed();
+            float noise = Mathf.PerlinNoise(Time.time, 0); //noise can also defined for y-axis for more variety
+            float delta = (moveSpeed + noise * perlinNoiseScale) *Time.deltaTime;
+            
+
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            transform.position += direction * delta;
+
+            if (Vector3.Distance(transform.position,targetPosition) <= delta/2)  //Epsilon value leads to overshoot and oscillation, fine-tuned threshold needed
+            {                                                                     // Vector2.MoveTowards inherently ensures the object will land exactly on the target position if the step size aligns perfectly.  
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
